df5b6aa6129d5c61a76198bf1fbfe513a27d84af
oakscode@gmail.com <oakscode@gmail.com>
2021-05-13 17:59:51 +0530

    last push

---
+++ clear_lock.sh
@@ -0,0 +1,8 @@
+sudo rm /var/lib/apt/lists/lock
+echo "Lock Folder Removed"
+sudo rm /var/cache/apt/archives/lock
+echo "Cache 1 Removed"
+sudo rm /var/lib/dpkg/lock
+echo "Cache 2 removed"
+echo "______________________"
+echo "DONE"
